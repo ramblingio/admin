932dfff1200bd2f19e0ca36947142401b819fbf2
chrkolst <christopher.kolstad@finn.no>
2016-07-05 18:54:48 +0200

    Configuring spring boot admin to use Eureka for discovery

--- src/main/java/io/rambling/AdminApplication.java
+++ src/main/java/io/rambling/AdminApplication.java
@@ -1,9 +1,13 @@
 package io.rambling;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class AdminApplication {
 
 	public static void main(String[] args) {
